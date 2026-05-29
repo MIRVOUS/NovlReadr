0bc0f157381865466c6af806f23cad77663c661a
Mustofa <45929526+MIRVOUS@users.noreply.github.com>
2026-05-29 13:16:52 +0700

    Update Scraper.kt

--- scraper/src/main/java/my/noveldokusha/scraper/Scraper.kt
+++ scraper/src/main/java/my/noveldokusha/scraper/Scraper.kt
@@ -5,20 +5,14 @@ import my.noveldokusha.scraper.databases.BakaUpdates
 import my.noveldokusha.scraper.databases.NovelUpdates
 import my.noveldokusha.scraper.sources.AT
 import my.noveldokusha.scraper.sources.BacaLightnovel
-/** import my.noveldokusha.scraper.sources.BestLightNovel */
-/** import my.noveldokusha.scraper.sources.BoxNovel */
 import my.noveldokusha.scraper.sources.IndoWebnovel
-/** import my.noveldokusha.scraper.sources.KoreanNovelsMTL */
 import my.noveldokusha.scraper.sources.LightNovelWorld
 import my.noveldokusha.scraper.sources.LightNovelsTranslations
 import my.noveldokusha.scraper.sources.LocalSource
-/** import my.noveldokusha.scraper.sources.MTLNovel */
 import my.noveldokusha.scraper.sources.MeioNovel
 import my.noveldokusha.scraper.sources.MoreNovel
 import my.noveldokusha.scraper.sources.NovelBin
 import my.noveldokusha.scraper.sources.NovelHall
-/** import my.noveldokusha.scraper.sources.Novelku */
-/** import my.noveldokusha.scraper.sources.ReadLightNovel */
 import my.noveldokusha.scraper.sources.ReadNovelFull
 import my.noveldokusha.scraper.sources.Reddit
 import my.noveldokusha.scraper.sources.RoyalRoad
@@ -26,9 +20,7 @@ import my.noveldokusha.scraper.sources.Saikai
 import my.noveldokusha.scraper.sources.SakuraNovel
 import my.noveldokusha.scraper.sources.Sousetsuka
 import my.noveldokusha.scraper.sources.WbNovel
-/** import my.noveldokusha.scraper.sources.Wuxia */
 import my.noveldokusha.scraper.sources.WuxiaWorld
-/** import my.noveldokusha.scraper.sources._1stKissNovel */
 import javax.inject.Inject
 import javax.inject.Singleton
 
